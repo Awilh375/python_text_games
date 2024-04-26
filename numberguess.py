2123d0be5390daa1e128ec6795160435608cffb8
Awilh_37 <159600155+Awilh375@users.noreply.github.com>
2024-04-26 14:06:47 -0400

    Create numberguess.py

---
+++ numberguess.py
@@ -0,0 +1,21 @@
+from random import randint as random
+max=int(input("What would you like the largest number to be?: "))
+number = random(0,max)
+guesses=0
+global guesses
+def choice():
+  guess=int(input("guess: "))
+  if guess!= number:
+    guesses=+1
+    if guess > number:
+      print("Too big. Try again.")
+    elif guess < number:
+      print("Too small. Try again.")
+    else:
+      print("Invalid guess. Please try again.")
+      guesses=-1
+  else:
+    print("Good job! The nubmer was "+number+". You got it in "+guesses+" guesses.")
+    break
+while True:
+  choice()
