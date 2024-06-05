cdf6d1b537dc022dd8b7ecf092b502b8484e453d
Awilh_37 <159600155+Awilh375@users.noreply.github.com>
2024-06-05 13:11:38 +0000

    updated numberguess.py, still doesn't work :(

--- numberguess.py
+++ numberguess.py
@@ -1,8 +1,10 @@
 from random import randint as random
 max=int(input("What would you like the largest number to be?: "))
 number = random(0,max)
-guesses=0
+global leave
+leave=0
 global guesses
+guesses=0
 def choice():
   guess=int(input("guess: "))
   if guess!= number:
@@ -15,7 +17,7 @@ def choice():
       print("Invalid guess. Please try again.")
       guesses=-1
   else:
-    print("Good job! The nubmer was "+number+". You got it in "+guesses+" guesses.")
-    break
-while True:
+    print("Good job! The nubmer was",str(number),". You got it in",str(guesses),"guesses.")
+    leave=1
+while leave==0:
   choice()
